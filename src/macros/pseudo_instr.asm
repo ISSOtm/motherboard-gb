2e813341a836002b21868ed8af09c8fd4482e92b
ISSOtm <eldredhabert0@gmail.com>
2019-03-07 15:27:04 +0100

    Improve `wait` macro on small args

--- src/macros/pseudo_instr.asm
+++ src/macros/pseudo_instr.asm
@@ -1,11 +1,17 @@
 
 ; Waits for \1 VBlanks
 wait: MACRO
-    ld c, \1
+    IF \1 < 6
+        REPT \1
+            rst wait_vblank
+        ENDR
+    ELSE
+        ld c, \1
 .delay\@
-    rst wait_vblank
-    dec c
-    jr nz, .delay\@
+        rst wait_vblank
+        dec c
+        jr nz, .delay\@
+    ENDC
 ENDM
 
 
