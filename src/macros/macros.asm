1f7e3005994b63e60c591b3a41c52353e19964a9
ISSOtm <eldredhabert0@gmail.com>
2019-06-30 17:19:00 +0200

    Use a submodule for rgbds-structs

--- src/macros/macros.asm
+++ src/macros/macros.asm
@@ -1,8 +1,8 @@
 
-INCLUDE "macros/structs.asm"
+INCLUDE "res/rgbds-structs/structs.asm"
 INCLUDE "macros/struct_decls.asm"
 INCLUDE "macros/pseudo_instr.asm"
 INCLUDE "macros/misc.asm"
 INCLUDE "macros/cutscene.asm"
 
- rgbds_structs_version 1.1.0
+ rgbds_structs_version 1.2.1
